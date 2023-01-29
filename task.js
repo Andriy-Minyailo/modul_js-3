5f3cffe5d9a8a106b28d26618a84805ea3366352
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-01-29 21:53:48 +0200

    add task_19

--- task.js
+++ task.js
@@ -124,7 +124,27 @@
   
 // }
 
-task_18
+// task_18
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+// for (const product  of products) {
+//   if (product.name === productName) {
+//     return product.price;
+//   }
+// }
+// return null;
+
+//   // Change code above this line
+// }
+
+// task_19
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -132,14 +152,19 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-function getProductPrice(productName) {
+function getAllPropValues(propName) {
   // Change code below this line
-for (const product  of products) {
-  if (product.name === productName) {
-    return product.price;
-  } 
-}
-return null;
+let arrayPropName = [];
+  for (const value of products) {
+    if (!value[propName]) {
+      return arrayPropName;
+    }
+    arrayPropName.push(value[propName]);
+  }
+  return arrayPropName;
 
   // Change code above this line
 }
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("sss"));
