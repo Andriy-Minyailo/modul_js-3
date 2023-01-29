160a67e7d7e0044450704710361f8ed68e9f7a56
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-01-29 15:25:15 +0200

    add task 11

--- task.js
+++ task.js
@@ -22,18 +22,37 @@
 // console.table(apartment);
 
 
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
 const keys = [];
 const values = [];
-// Change code below this line
+const advert = {
+  service: "apt",
+};
+const apartment = Object.create(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
 for (const key in apartment) {
+  // Change code below this line
+if (apartment.hasOwnProperty(key)) {
   keys.push(key);
   values.push(apartment[key]);
 }
-
-console.log(keys);
-console.log(values);
+  // Change code above this line
+}
