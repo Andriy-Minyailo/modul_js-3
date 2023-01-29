5bbbc838e551f576a01b0c1188f77a0e3e05b6b3
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-01-29 15:19:17 +0200

    add task 10

--- task.js
+++ task.js
@@ -1,22 +1,39 @@
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
 
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//     country: 111,
+// city: "Kingston", 
+// }
+
+// console.table(apartment);
+
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
 // Change code below this line
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = {
-    country: 111,
-city: "Kingston", 
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
 }
 
-console.table(apartment);
+console.log(keys);
+console.log(values);
